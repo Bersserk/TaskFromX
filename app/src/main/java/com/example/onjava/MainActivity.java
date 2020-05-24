8ff37bef58a42588b72955563ea0f9532ef6e89f
Bersserk <bers.developer@gmail.com>
2020-05-24 20:25:08 +0200

    new commit

--- app/src/main/java/com/example/onjava/MainActivity.java
+++ app/src/main/java/com/example/onjava/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.onjava;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    Button start, stop;
+    public static TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        text = findViewById(R.id.text);
+
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
     }
+
+    public static void newText(String s){
+        text.setText(s);
+    }
+
+
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.start:
+                //this.text.setText("сервис запущен");
+                //Log.d("LOG", "text из MainActivity " + text.getText());
+                startService(new Intent(this, MyService.class));
+
+                break;
+            case R.id.stop:
+                //text.setText("Нажата кнопка Button2");
+                stopService(new Intent(this, MyService.class));
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/onjava/MyService.java
@@ -0,0 +1,84 @@
+package com.example.onjava;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyService extends Service {
+
+
+
+    Boolean stop = false;
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        setTime ();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("LOG", "destroy");
+        stop = true;
+
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void setTime() {
+
+        //MainActivity.text.setText("new text");
+        Log.d("LOG", "new text - " + MainActivity.text.getText() );
+
+        Log.d("LOG", "setTime" );
+
+        new Thread(new Runnable() {
+            public void run() {
+                Log.d("LOG", "run" );
+                for (int i = 0; i > -1 ; i++) {
+                    if(stop)
+                        break;
+
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                        MainActivity.newText("newtext");
+
+                        Log.d("LOG", "try " + i);
+                        //MainActivity.text.setText("text");
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("LOG", "while");
+
+
+                }
+                Log.d("LOG", "вышли с цикла");
+                stopSelf();
+            }
+        }).start();
+    }
+
+
+}
